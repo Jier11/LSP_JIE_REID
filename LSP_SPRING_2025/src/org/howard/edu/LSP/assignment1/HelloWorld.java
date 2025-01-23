2e14d6d1347a16b5fbb2b7dbc53f19c1016ba978
Jie Reid <jiereid1@gmail.com>
2025-01-22 20:47:50 -0500

    Second time

--- LSP_SPRING_2025/src/org/howard/edu/LSP/assignment1/HelloWorld.java
+++ LSP_SPRING_2025/src/org/howard/edu/LSP/assignment1/HelloWorld.java
@@ -1,9 +1,12 @@
 package org.howard.edu.LSP.assignment1;
 
 public class HelloWorld {
-
+	/**
+	 * Jie Reid
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 System.out.println("Hello, World!");
 
 	}
 
